d5aaec2ba655a837affc92dc4d0c3ef14ecaa1b6
czq <592012072@qq.com>
2025-04-10 15:58:57 +0800

    修复网关不能启动的bug

--- ai-avatar-backend/ai-avatar-gateway/src/main/java/com/aiavatar/gateway/GatewayApplication.java
+++ ai-avatar-backend/ai-avatar-gateway/src/main/java/com/aiavatar/gateway/GatewayApplication.java
@@ -2,8 +2,10 @@ package com.aiavatar.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = "com.aiavatar")
+@EnableDiscoveryClient
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
